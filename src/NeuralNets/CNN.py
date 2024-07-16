18c24d727b0319a88a567782de191b0fe5c8abfc
Bilal Aabkari <bilalaabkari13@gmail.com>
2024-07-17 01:13:20 +0200

    reached 87% accuracy

--- src/NeuralNets/CNN.py
+++ src/NeuralNets/CNN.py
@@ -28,24 +28,32 @@ class CustomResNet34(nn.Module):
 class ConvNetV1(nn.Module):
     def __init__(self, size):
         super(ConvNetV1, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, 5, padding=2)
-        self.bn1 = nn.BatchNorm2d(8)
+        self.conv1 = nn.Conv2d(1, 16, 5, padding=2)
+        self.bn1 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(2, 2)
 
-        self.conv2 = nn.Conv2d(8, 16, 5, padding=2)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         self.pool2 = nn.MaxPool2d(2, 2)
 
-        self.conv3 = nn.Conv2d(16, 32, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
         self.pool3 = nn.MaxPool2d(2, 2)
-        
-        self.conv4 = nn.Conv2d(32, 64, 3, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
 
-        self.linear1 = nn.Linear(64*32*32, 1024)
-        self.linear2 = nn.Linear(1024, 512)
-        self.linear3 = nn.Linear(512, 75)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.pool5 = nn.MaxPool2d(2, 2)
+        
+        self.conv6 = nn.Conv2d(256, 512, 3, padding=1)
+        self.bn6 = nn.BatchNorm2d(512)
+
+        self.linear1 = nn.Linear(512*8*8, 1024)
+        self.dropout = nn.Dropout(p=0.5)
+        self.linear2 = nn.Linear(1024, 75)
 
     def forward(self, x):
         show_shapes = False
@@ -62,20 +70,27 @@ class ConvNetV1(nn.Module):
         if show_shapes:
             print(x.shape)
 
-        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+        if show_shapes:
+            print(x.shape)
+
+        x = self.pool5(F.relu(self.bn5(self.conv5(x))))
+        if show_shapes:
+            print(x.shape)
+
+        x = F.relu(self.bn6(self.conv6(x)))
         if show_shapes:
             print(x.shape)
 
         x = torch.flatten(x, 1)
         x = F.relu(self.linear1(x))
+        x = self.dropout(x)
         if show_shapes:
             print(x.shape)
 
-        x = F.relu(self.linear2(x))
-        if show_shapes:
-            print(x.shape)
+    
 
-        x = self.linear3(x)
+        x = self.linear2(x)
         if show_shapes:
             print(x.shape)
 
